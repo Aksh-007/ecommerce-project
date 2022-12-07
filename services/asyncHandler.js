e6bb230318ec6a1686849febeb8982fe847a8605
Aksh-007 <akshaybarapatre54@gmail.com>
2022-12-07 19:18:47 +0530

    add service folder and in that added asynchandler

---
+++ services/asyncHandler.js
@@ -0,0 +1,7 @@
+// in controller many method will call the database so we have to write async await each time database call is there so creating a asynchandler so that we dont want to repeat async awiat 
+
+const asynchandler = () =>{
+    
+}
+
+export default asynchandler;
