39205f5175f6139a952d88980982d04fe5c76938
Aksh-007 <akshaybarapatre54@gmail.com>
2022-12-07 17:12:56 +0530

    added product schema and refernce of other schema

--- models/product.schema.js
+++ models/product.schema.js
@@ -2,9 +2,52 @@ import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema(
     {
-        productName :{
-            type:String,
-        }
+        productName: {
+            type: String,
+            required: [true, "Product name is required"],
+            trim: true,
+            maxLength: [120, "Product name should be a max 120characters"]
+        },
+        price: {
+            type: Number,
+            required: [true, "Price is required"],
+            maxLength: [5, "product price is not greater than 5 digit"]
+        },
+        productDescription: {
+            type: String,
+            //use some form of editior - personal assingment
+        },
+        productPhoto: [
+            {
+                url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        productStock: {
+            type: Number,
+            default: 0
+        },
+        productSold: {
+            type: Number,
+            default: 0
+        },
+        //WHAT WE ARE DOING -  each product should be a part of collection/category
+        // I WANT TO STORE REFERNECE OF COLLECTION
+        //REFERNECE OF ANOTHER SCHEMA
+        // why we are doing bcos product is of different category like winter wear summer wear
+        // category in phone like apple , samasung, nokia,xiaomi, poco
+        collectionId: {
+            // ObjectId is used to store ids 
+            type:mongoose.Schema.Types.ObjectId,
+            // refernece of another schema
+            ref:"Collection"
+        } 
+
+    },
+    {
+        timestamps: true
     }
 );
 
