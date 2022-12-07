367a8d7e7c7f45746e0eba70fa0265f97985978d
Aksh-007 <akshaybarapatre54@gmail.com>
2022-12-07 21:49:37 +0530

    added middleware morgan,cors,cookie-parser,express

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 const app = express();
 
 //middleware
-app.use
+app.use(express.json());
+// when we want to use form data we use urlencoded
+app.use(express.urlencoded({ extended: true }));
+// never put * in cors
+app.use(cors());
+// so we can access of cookies
+app.use(cookieParser());
+// morgan is a logger we can use many method like combined , tiny so many are there
+app.use(morgan('tiny'));
 
 
 
