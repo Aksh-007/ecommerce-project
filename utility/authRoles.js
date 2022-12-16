50d2a45b881b896417ab8aba5d69d91a8661e0c1
Aksh-007 <akshaybarapatre54@gmail.com>
2022-12-16 16:13:10 +0530

    added authroles comment

--- utility/authRoles.js
+++ utility/authRoles.js
@@ -1,4 +1,6 @@
-    const authRoles = {
+   //this is an object in that having key value pair and creating kind of enum we are having const like ADMIN : "ADMIN" and we mant to add more roles then we can add like superadmin and all
+   
+   const authRoles = {
         ADMIN: "ADMIN",
         MODERATOR: "MODERATOR",
         USER: "USER"
