65a97868f559f60255ccbe36cec60d6da803e2d3
Aksh-007 <akshaybarapatre54@gmail.com>
2023-01-20 11:37:37 +0530

    added add product api with image upload

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -6,6 +6,8 @@ import { s3FileUpload, deleteFile } from '../services/imageUpload.js';
 import Mongoose from 'mongoose';
 import asyncHandler from "../services/asyncHandler";
 import customError from "../utility/customError";
+import Mongoose from 'mongoose';
+import config from '../config/index.js'
 
 /**********************************************************
  * @ADD_PRODUCT
@@ -17,5 +19,48 @@ import customError from "../utility/customError";
  *********************************************************/
 
 export const addProduct = asyncHandler(async (req, res) => {
-    
+    const form = formidable({
+        multiples: true,
+        keepExtensions: true
+    });
+
+    form.parse(req, async function (err, fields, files) {
+        try {
+            if (err) {
+                throw new customError(err.message || "something went wrong", 500)
+            }
+            //generating id in mongodb using mongoose and store it in database
+            let productId = new Mongoose.Types.ObjectId().toHexString();
+            // console.log(fields, files);
+            //check for fields
+            if (!fields.name ||
+                !fields.price ||
+                !fields.productDescription ||
+                !fields.collectionId
+            ) {
+                throw new customError("please fill all details", 500);
+            }
+
+            // handling images
+            let imageArrayResponse = Promise.all(
+                Object.keys(files).map(async (filekey, index) => {
+                    const element = files[filekey]
+                    const data = fs.readFileSync(element.filepath);
+                    const upload = await s3FileUpload({
+                        bucketName: config.S3_BUCKET_NAME,
+                        key: `products/${productId}/photo_${index + 1}.png`,
+                        body: data,
+                        contentType: element.mimetype
+                    })
+                    return
+                })
+            )
+
+        } catch (error) {
+            return res.status(500).json({
+                sucess: false,
+                message: error.message || `Something went wrong`
+            })
+        }
+    })
 });
